9016ee60701b9ca3c4ff26749d1ac6b7f0868f16
VukV <vuk.vukovic.2000@gmail.com>
2023-11-21 15:59:52 +0100

    working on login/register + refactoring

--- mini_github/settings.py
+++ mini_github/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(os.path.join(BASE_DIR, 'apps'))  # find all apps in /apps
+# sys.path.append(os.path.join(BASE_DIR, 'apps'))  # find all apps in /apps
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.user'
+    'apps.auth'
 ]
 
 MIDDLEWARE = [
