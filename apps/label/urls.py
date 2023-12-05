c067dd97c90a93163dea9d9a2d487647121e580c
VukV <vuk.vukovic.2000@gmail.com>
2023-12-05 23:03:06 +0100

    initial labels page

--- apps/label/urls.py
+++ apps/label/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.authentication import views
+from apps.label import views
 
 
 urlpatterns = [
-
+    path('<int:repository_id>/', views.labels_from_repository, name='repository_labels'),
 ]
--- apps/label/views.py
+++ apps/label/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from apps.repository.models import Repository
+
+
+@login_required()
+def labels_from_repository(request, repository_id):
+    repository = get_object_or_404(Repository, id=repository_id)
+
+    if not repository.check_access(request.user):
+        error_message = 'You do not have access to this repository.'
+        return render(request, 'error.html', {'error_message': error_message})
+
+    labels = repository.labels.all()
+    render_object = {
+        'repository': repository,
+        'labels': labels
+    }
+
+    return render(request, 'repository/repository_labels.html', render_object)
+
+
+@login_required()
+def add_label(request, repository_id):
+    # Logic to handle adding a new label
+    pass
+
+
+@login_required()
+def edit_label(request, repository_id, label_id):
+    # Logic to handle label editing
+    pass
+
+
+@login_required()
+def delete_label(request, repository_id, label_id):
+    # Logic to handle label deletion
+    pass
