abbccc34721152189387c28ce811bfa0b8747fb4
VukV <vuk.vukovic.2000@gmail.com>
2023-12-12 15:49:11 +0100

    issue details

--- apps/issue/urls.py
+++ apps/issue/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('<int:repository_id>/', views.issues_from_repository, name='repository_issues'),
     path('<int:repository_id>/<int:issue_id>', views.issue_detail, name='issue_detail'),
     path('<int:repository_id>/create_issue', views.add_issue, name='add_issue'),
+    path('<int:repository_id>/close_issue/<int:issue_id>', views.close_issue, name='close_issue'),
+    path('<int:repository_id>/edit_issue/<int:issue_id>', views.add_issue, name='edit_issue'),
+    path('<int:repository_id>/delete_issue/<int:issue_id>', views.add_issue, name='delete_issue'),
 ]
--- apps/issue/views.py
+++ apps/issue/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from apps.history.models import HistoryType, ChangeAction
 from apps.issue.forms import IssueForm
+from apps.issue.models import Issue
 from apps.label.models import Label
 from apps.milestone.models import Milestone
 from apps.project.models import Project
@@ -29,7 +30,15 @@ def issues_from_repository(request, repository_id):
 
 @login_required()
 def issue_detail(request, repository_id, issue_id):
-    pass
+    repository = get_object_or_404(Repository, id=repository_id)
+
+    if not repository.check_access(request.user):
+        error_message = 'You do not have access to this repository.'
+        return render(request, 'error.html', {'error_message': error_message})
+
+    issue = get_object_or_404(Issue, pk=issue_id, repository=repository)
+
+    return render(request, 'repository/issues/issue_details.html', {'repository': repository, 'issue': issue})
 
 
 @login_required()
@@ -55,19 +64,19 @@ def add_issue(request, repository_id):
 
             if project and project.repository != repository:
                 error_message = 'Selected project is not linked to this repository.'
-                return create_issue_error(request, error_message, form, repository)
+                return issue_error(request, error_message, form, repository)
 
             if milestone and milestone.repository != repository:
                 error_message = 'Selected milestone is not linked to this repository.'
-                return create_issue_error(request, error_message, form, repository)
+                return issue_error(request, error_message, form, repository)
 
             if assignees and not all(assignee in repository.collaborators.all() for assignee in assignees):
                 error_message = 'One or more selected assignees are not collaborators of this repository.'
-                return create_issue_error(request, error_message, form, repository)
+                return issue_error(request, error_message, form, repository)
 
             if labels and not all(label in repository.labels.all() for label in labels):
                 error_message = 'One or more selected labels are not linked to this repository.'
-                return create_issue_error(request, error_message, form, repository)
+                return issue_error(request, error_message, form, repository)
 
             issue.save()
             form.save_m2m()
@@ -94,7 +103,22 @@ def add_issue(request, repository_id):
     return render(request, 'repository/issues/add_issue.html', {'form': form, 'repository': repository})
 
 
-def create_issue_error(request, message, form, repository):
+@login_required()
+def close_issue(request, repository_id, issue_id):
+    pass
+
+
+@login_required()
+def edit_issue(request, repository_id, issue_id):
+    pass
+
+
+@login_required()
+def delete_issue(request, repository_id, issue_id):
+    pass
+
+
+def issue_error(request, message, form, repository):
     render_object = {
         'error_message': message,
         'form': form,
