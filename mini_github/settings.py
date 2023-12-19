666cc1946de8d132fd435318df97abf4482fa792
VukV <vuk.vukovic.2000@gmail.com>
2023-12-19 19:52:55 +0100

    prod boolean

--- mini_github/settings.py
+++ mini_github/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import sys
 import os
 
-PROD = False
+PROD = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
