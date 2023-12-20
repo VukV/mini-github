c29d24a6f286a6d2a5b746291ceaa36255f77cc4
VukV <vuk.vukovic.2000@gmail.com>
2023-12-20 23:06:14 +0100

    branches CRUD finished

--- apps/branch/urls.py
+++ apps/branch/urls.py
@@ -5,6 +5,6 @@ from apps.branch import views
 urlpatterns = [
     path('<int:repository_id>/', views.branches_from_repository, name='repository_branches'),
     path('<int:repository_id>/create_branch/', views.add_branch, name='add_branch'),
-    path('<int:repository_id>/delete_branch/<int:branch_id>', views.edit_branch, name='edit_branch'),
-    path('<int:repository_id>/edit_branch/<int:branch_id>', views.delete_branch, name='delete_branch'),
+    path('<int:repository_id>/edit_branch/<int:branch_id>', views.edit_branch, name='edit_branch'),
+    path('<int:repository_id>/delete_branch/<int:branch_id>', views.delete_branch, name='delete_branch'),
 ]
