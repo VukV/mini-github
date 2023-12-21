7573c1af6ab3739564dce9bb18b0397ee45a5be3
VukV <vuk.vukovic.2000@gmail.com>
2023-12-21 13:58:37 +0100

    adding commits

--- apps/commit/views.py
+++ apps/commit/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
+import hashlib
+import datetime
+
 from apps.branch.models import Branch
 from apps.commit.forms import CommitForm
 from apps.history.models import HistoryType, ChangeAction
@@ -29,22 +32,25 @@ def commits_from_branch(request, branch_id):
 @login_required()
 def add_commit(request, branch_id):
     branch = get_object_or_404(Branch, pk=branch_id)
-
     repository = branch.repository
+
     if not repository.check_access(request.user):
         error_message = 'You do not have access to this repository.'
         return render(request, 'error.html', {'error_message': error_message})
 
     if request.method == 'POST':
         form = CommitForm(request.POST)
-
         if form.is_valid():
             commit = form.save(commit=False)
             commit.author = request.user
             commit.repository = repository
-            # TODO add branch
-            # TODO create hash
+
+            hash_source = f"{commit.message}-{commit.repository.name}-{datetime.datetime.now()}"
+            commit_hash = hashlib.sha256(hash_source.encode()).hexdigest()
+            commit.hash = commit_hash
+
             commit.save()
+            commit.branches.add(branch)
 
             utils.create_history_item(
                 user=request.user,
@@ -54,16 +60,20 @@ def add_commit(request, branch_id):
                 changed_name=branch.name
             )
 
-            return redirect('TODO', repository_id=repository.id)
+            return redirect('branch_commits', branch_id=branch.id)
         else:
-            error_message = 'Invalid form.'
             render_object = {
-                'error_message': error_message,
+                'error_message': 'Invalid form.',
                 'form': form,
-                'repository': repository
+                'repository': repository,
+                'branch': branch
             }
-            return render(request, 'TODO', render_object)
+            return render(request, 'repository/commits/add_commit.html', render_object)
     else:
         form = CommitForm()
 
-    return render(request, 'TODO', {'form': form, 'repository': repository})
+    return render(request, 'repository/commits/add_commit.html', {
+        'form': form,
+        'repository': repository,
+        'branch': branch
+    })
